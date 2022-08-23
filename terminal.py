f163d69b1d6716735af53c0ed98a267655d7d7af
RathoreAgastya <78737482+RathoreAgastya@users.noreply.github.com>
2022-08-23 14:29:30 +0530

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,42 @@
+
+from rich.console import Console
+import time
+from PIL import Image
+from pygame import mixer
+from rich.traceback import install
+from rich.progress import track
+console = Console()
+mixer.init()
+install()
+
+rick = mixer.Sound("RICK_ROLL_BABYYYYYYYYYYY.mp3")
+rick.set_volume(3)
+img = Image.open("RICK.jpg")
+
+console.input("WElCOME TO AGASTINAL!\nPress enter to continue")
+
+for i in track(range(50), description="Loding terminal...") :
+    console.print("Loading terminal layout...")
+    # time.sleep(.2)
+for x in track(range(10), description="Loading commands...") :
+    console.print("Loading python interpreter...")
+    # time.sleep(.2)
+for y in track(range(70), description="Loding all packages...") :
+    console.print("Loading python modules...")
+    console.print("Loading donut...")
+    # time.sleep(.2)
+
+input = console.input("[yellow]$[/yellow] ")
+
+if input == ">:)" :
+    rick.play()
+    img.show()
+
+elif input == "python hello" :
+    console.print('print("Hello, world!")', style="bold green")
+
+elif input == "C hello" :
+    console.print('#include <stdio.h>\n\nint main()\n{\n    printf("Hello, world!");\n   return 0;', style="bold green")
+
+elif input == "java hello" :
+    console.print('\nclass hello {\n    [bold green]public static void[/] main(String[]) {\n       System.out.println("Hello, world!");\n  }\n}')
